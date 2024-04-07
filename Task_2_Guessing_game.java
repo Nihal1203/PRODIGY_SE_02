87eab581a08a50e4259cd25c8f063d0ca8bb7451
Nihal1203 <118458326+Nihal1203@users.noreply.github.com>
2024-04-07 11:19:07 +0530

    Add files via upload

---
+++ Task_2_Guessing_game.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Task_2_Guessing_game {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int rannum = random.nextInt(99) + 1;
+        System.out.println("Enter the Guess Number");
+        int t=0,attempt=0;
+        while(rannum != t){
+            t=sc.nextInt();
+            attempt = attempt+1;
+            if(t>rannum)
+                System.out.println("Guess Number is higher then Random number. Guess again! ");
+
+            else if(t<rannum)
+                System.out.println("Guess Number is Lesser then Random number. Guess again! ");
+
+            else{
+                System.out.println("Guess Number is Equal to Random number.  ");
+                System.out.println("You Won! ");
+                break;
+            }
+        }
+        System.out.println("Number of Attempts: " + attempt);
+        sc.close();
+    }
+}
